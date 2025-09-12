6c8b675b26b7e6d001bca1e1054fa9fbe3a31b83
christianelcompanero <cevp1505@gmail.com>
2025-09-12 17:30:32 -0400

    Create diccionario.py
    primer proyecto

---
+++ diccionario.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY":  "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+            }
+for i in range(5):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+    if word in meme_dict.keys():
+        print("esa es una de las palabras")
+        
+    else:
+        print("no se encuentra esa palabra")
+        
