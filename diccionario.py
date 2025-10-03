84509c33502594a4bf079c5e2ad88c03235b70dd
christianelcompanero <cevp1505@gmail.com>
2025-10-03 17:22:13 -0400

    Update diccionario.py

--- diccionario.py
+++ diccionario.py
@@ -1,17 +1,20 @@
-meme_dict = {
-            "CRINGE": "Algo excepcionalmente raro o embarazoso",
-            "LOL": "Una respuesta común a algo gracioso",
-            "ROFL": "una respuesta a una broma",
-            "SHEESH": "ligera desaprobación",
-            "CREEPY":  "aterrador, siniestro",
-            "AGGRO": "ponerse agresivo/enojado"
-            }
-for i in range(5):
-    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
 
-    if word in meme_dict.keys():
-        print("esa es una de las palabras")
-        
-    else:
-        print("no se encuentra esa palabra")
-        
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
